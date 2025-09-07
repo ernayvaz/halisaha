8da6486339604b6651b2146a31a47896154f4153
Eren Ayvaz <e.ayvaz@raynet.de>
2025-09-07 23:45:46 +0300

    Final fix for database and environment issues

--- prisma/migrations/20250831121426_pregame_notifs/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "public"."Event" ADD COLUMN     "preNotify1hSent" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "preNotify2hSent" BOOLEAN NOT NULL DEFAULT false,
-ADD COLUMN     "preNotify3hSent" BOOLEAN NOT NULL DEFAULT false;
